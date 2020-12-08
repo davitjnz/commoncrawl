3ba56c573ec471d55caac83fa2d89c614b21f357
Davit <DatoJanez@users.noreply.github.com>
2020-12-08 23:44:50 +0400

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,8 +1,18 @@
-def collect(terminated_file):
+from glob import glob
+from tqdm.notebook import tqdm
+from datetime import datetime
+import json
+from os import system
+import argparse
+import subprocess
 
-    index_file_batch_path_ = glob('/content/drive/MyDrive/commoncrowl/aze*')[0] if not terminated_file else '/content/drive/MyDrive/commoncrowl/__{}.tar.gz'.format(terminated_file)
+def collect(continue_with_file=None):
+
+    index_file_batch_path_ = glob('/content/drive/MyDrive/commoncrowl/aze*')[0] if not continue_with_file else '/content/drive/MyDrive/commoncrowl/__{}.tar.gz'.format(continue_with_file)
     index_file_batch_path  = index_file_batch_path_.replace('commoncrowl/aze', 'commoncrowl/__aze').replace('commoncrowl/arm', 'commoncrowl/__arm')
-    system("mv {} {}".format(index_file_batch_path_, index_file_batch_path))
+    if not continue_with_file:
+        system("mv {} {}".format(index_file_batch_path_, index_file_batch_path))
+    
     batch_name = index_file_batch_path_.split('/')[-1].replace('.tar.gz', '')
     batch_name = batch_name.replace('__', '')
     print(batch_name)
@@ -15,7 +25,7 @@ def collect(terminated_file):
     system("rm /content/indexes/*")
 
     lines_lenght_ = subprocess.getoutput("wc -l /content/indexes.txt")
-    lines_lenght = int(lines_lenght_[0].split(' ')[0])
+    lines_lenght = int(lines_lenght_.split(' ')[0])
 
     start_date = datetime.now()
 
@@ -24,7 +34,7 @@ def collect(terminated_file):
     existing = [int(a.replace('.tar.gz', '').split('-')[-1]) for a in glob('/content/drive/MyDrive/commoncrowl-data/{}*'.format(batch_name))]
     start_from = 0 if len(existing) == 0 else max(existing)
 
-    print('starting from:', start_from)
+    print('starting from', start_from, 'of', lines_lenght)
 
     for line_index in range(lines_lenght):
         if line_index < start_from + 1:
@@ -33,19 +43,18 @@ def collect(terminated_file):
         line = index_file.readline()
 
         index = json.loads('{"url":' + line.split('{"url":')[1])
-        
-        system("curl -s -r{}-$(({}+{}-1)) https://commoncrawl.s3.amazonaws.com/{} > tmp.warc.gz".format(index["offset"], index["offset"], index["length"], index["filename"])) 
-        system("warcio extract --payload tmp.warc.gz 0 > tmp.html")
-        system("lynx --dump tmp.html > /content/text/tmp{}.txt".format(str(line_index)))
 
-        system("rm *.html ")
-        system("rm *.warc.gz")
+        system("curl -s -r{}-$(({}+{}-1)) https://commoncrawl.s3.amazonaws.com/{} > /content/tmp.warc.gz".format(index["offset"], index["offset"], index["length"], index["filename"])) 
+        system("warcio extract --payload /content/tmp.warc.gz 0 > tmp.html")
+        system("lynx --dump /content/tmp.html > /content/text/tmp{}.txt".format(str(line_index)))
+
+        system("rm /content/*.html ")
+        system("rm /content/*.warc.gz")
 
         system("cat /content/text/*.txt > /content/text/_tmp")
         system("rm /content/text/*.txt")
         system("mv /content/text/_tmp /content/text/extracted-{}.txt".format(str(line_index)))
-        
-    
+            
         if line_index != 0 and line_index % 3000 == 0 or line_index == lines_lenght - 1:
             passed = (datetime.now()-start_date).seconds/60
             print(line_index, '/', lines_lenght, '    ', round(passed, 2), '/', round(passed *(lines_lenght - start_from)/(line_index - start_from), 2))
@@ -62,5 +71,12 @@ def collect(terminated_file):
     system("rm /content/text/*")
     
 
-    collect(False)
-    
+    collect()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Collect Data From CommonCrawl')
+    parser.add_argument('--continue_with_file', help='File to continue with', )
+    args = parser.parse_args()
+
+    collect(args.continue_with_file)
